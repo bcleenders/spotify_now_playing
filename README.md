1667890a4d20fb461a566fa8542bfb25f98583cb
bcleenders <bcleenders@gmail.com>
2022-01-17 21:16:56 -0500

    Commit 1 - draw a QR code as example

---
+++ README.md
@@ -0,0 +1,3 @@
+# ESP32 Spotify now playing
+
+This repo fetches the currently playing track from your Spotify account, and shows it on an e-ink display.
---
+++ src/Constants.h
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_H_DEFINED
+#define CONSTANTS_H_DEFINED
+
+class Constants {
+   public:
+    static const char *ssid;
+    static const char *password;
+    static const char *ntpServer;
+};
+
+#endif
